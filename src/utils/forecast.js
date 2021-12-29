4813a0eefde7d08e694339ab010f8afe94b88dc2
Amarjeet Kaur <amarjeet.kaur@cybergindia.local>
2021-12-29 19:24:51 +0530

    changing units of temperature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=ef75e85b4994e8d3298b2594c97e578c&query=' + latitude + ',' + longitude + '&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=ef75e85b4994e8d3298b2594c97e578c&query=' + latitude + ',' + longitude
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
